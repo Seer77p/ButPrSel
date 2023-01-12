27583e125ea35cacffc39bbba94d3ab991f6193f
Seer77p <seer7703@mail.ru>
2023-01-12 13:40:46 +0300

    Sorting

---
+++ SelectionSort/Sorting.cs
@@ -0,0 +1,21 @@
+public static class Sorting
+{
+//public static void SortSelection(int[] collection) 
+public static int[] SortSelection(this int[] collection) //this добавляем для записи "10.SortSelection();" 
+                                                        //int[] меняем для возврата значений
+{
+    int size = collection.Length;
+    for (int i = 0; i < size; i++)
+    {
+       int pos=i;
+       for (int j = i+1; j < size; j++)
+       {
+        if(collection[j]<collection[pos]) pos=j;
+       } 
+       int temp = collection[i];
+       collection[i] = collection[pos];
+       collection[pos]=temp;
+    }
+    return collection; //Возвращаем занчение
+}
+}
