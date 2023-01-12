a2d571ea3490c8f68ac1afd9e6fdd8a92ad87133
Seer77p <seer7703@mail.ru>
2023-01-12 13:39:31 +0300

    Основной

--- SelectionSort/Program.cs
+++ SelectionSort/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static Sorting; //Дает возможность пользоваться функцией из файла Sirting.cs
+using static Infrastructure; //Дает возможность пользоваться функцией из файла Infrastructure.cs
+
+// int[] array = CreateArray(10);  // Классическая запись
+// Print(array);
+// SortSelection(array);
+// Print(array);
+
+// 10.CreateArray() //Запись при добавлении this в функциях
+//   .Print()
+//   .SortSelection()
+//   .Print();
+
+//10.CreateArray().Print().SortSelection().Print(); аналогичная запись (Запись при добавлении this в функциях)
+
+//   10.CreateArray(10,15) //Запись при добавлении this в функциях (задаем min и max вручную)
+//   .Print()
+//   .SortSelection()
+//   .Print();
+  10.CreateArray(10,200) 
+  .Print(" | ")//изменяем для редактировния вывода в консоль
+  .SortSelection()
+  .Print(" <> ");//изменяем для редактировния вывода в консоль
