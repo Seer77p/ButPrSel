4419c386353f257e9928647a5907a6892ed416b9
Seer77p <seer7703@mail.ru>
2023-01-12 14:36:33 +0300

    Готовый проект с коментариями

--- SelectionSort/Infrastructure.cs
+++ SelectionSort/Infrastructure.cs
@@ -4,6 +4,13 @@ using static System.String; // Позволяет не писать "String"
 public static class Infrastructure // Заполняет массив случайными числами
 {
     //public static int[] CreateArray(int size, int min=0, int max=10)
+    /// <summary>
+    /// Метод, создания и заполнения массива
+    /// </summary>
+    /// <param name="size">Размер нового массива</param>
+    /// <param name="min">Нижняя граница заполнения</param>
+    /// <param name="max">Верхняя граница заполнения</param>
+    /// <returns>Новый массив</returns>
     public static int[] CreateArray(this int size, int min=0, int max=10) // Добавляем this для записи "10.CreateArray();"
     {
         return Enumerable.Range(1, size)
@@ -12,6 +19,12 @@ public static class Infrastructure // Заполняет массив случа
     }
     //public static void Print(int[] array) //Печатает массив
     //public static int[] Print(this int[] array) // добавляем this для записи ".Print();"
+    /// <summary>
+    /// Печать массива в консоль
+    /// </summary>
+    /// <param name="array">Исходный массив</param>
+    /// <param name="separator">Символ-разделитель элементов массива</param>
+    /// <returns>Исходный массив</returns>
     public static int[] Print(this int[] array, string separator = ",") // изменяем для редактировния вывода в консоль
     {
         string output = Join(separator, array);//изменяем для редактировния вывода в консоль
--- SelectionSort/Sorting.cs
+++ SelectionSort/Sorting.cs
@@ -1,6 +1,11 @@
 public static class Sorting
 {
 //public static void SortSelection(int[] collection) 
+/// <summary>
+/// Сортировка методом выбора
+/// </summary>
+/// <param name="collection">Исходный массив</param>
+/// <returns>Отсортированный массив</returns>
 public static int[] SortSelection(this int[] collection) //this добавляем для записи "10.SortSelection();" 
                                                         //int[] меняем для возврата значений
 {
